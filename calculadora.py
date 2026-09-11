1d56ef9c9b8269efa38a8602573e5ba21f97427a
jobson <otakupokego@gmail.com>
2026-09-11 19:30:28 -0300

    adicionado a multiplicação

--- calculadora.py
+++ calculadora.py
@@ -17,3 +17,9 @@ def divisão(num1, num2):
     return divisao
 
 print("Resultado da divisão: ", divisão(5, 3))
+
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+
+print("Resultado da multiplicacao: ", multiplicacao(5, 3))
