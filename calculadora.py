febed2770e1f5bf13b0cad04f997c989ec7259b9
jobson <otakupokego@gmail.com>
2026-09-11 19:23:52 -0300

    adicionado a subtração

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,9 @@ def soma(num1, num2):
     return soma
 
 print("Resultado de la soma: ", soma(5, 3))
+
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+print("Resultado da subtracao: ", subtracao(5, 3))
